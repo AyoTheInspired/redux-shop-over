7afbc7c37734167e67804ca4b174b6845f344db9
Ayo <bimboladxtreme@gmail.com>
2021-08-25 22:43:24 +0100

    Item Cards are done

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { Card, Col } from "react-bootstrap";
 import styled from "styled-components";
 import { IconButton } from "@material-ui/core";
+import StarRatingComponent from "react-star-rating-component";
 
 function ProductComponent() {
 	const products = useSelector(selectSetProducts);
@@ -23,8 +24,8 @@ function ProductComponent() {
 
 				return (
 					<Col lg={3} md={5} sm={4} key={id} className="my-3">
-						<Wrap className="flex-col mx-3">
-							<div className="productImg__wrap pt-2">
+						<Wrap className="d-flex flex-column mx-3">
+							<div className="productImg__wrap flexed pt-2">
 								<img
 									className="product__image"
 									src={image}
@@ -33,7 +34,7 @@ function ProductComponent() {
 									height="150"
 								/>
 							</div>
-							<div className="product__details px-2">
+							<div className="product__details p-2 pb-1">
 								<div className="details__top flex-btw">
 									<p className="product__title mb-0 text-center">
 										{truncate(title, 25)}
@@ -42,10 +43,23 @@ function ProductComponent() {
 										<AddShoppingCart className="items__cartIcon" />
 									</IconButton>
 								</div>
-								<div className="details__mid">
+								<div className="details__mid flex-btw">
 									<p className="mb-0 product__price">${price}</p>
+									<StarRatingComponent
+										name={title}
+										className="star__rating"
+										starCount={5}
+										value={3}
+									/>
 								</div>
-								<div className="details__bottom"></div>
+								<div className="details__bottom">
+									{/* <p className="mb-0 text-capitalize text-center">{category}</p> */}
+									<Link to={`/product/${id}`} className="product__link">
+										<p className="mb-0 product__more text-center my-2 py-2">
+											More Details
+										</p>
+									</Link>
+								</div>
 							</div>
 						</Wrap>
 					</Col>
@@ -78,9 +92,17 @@ export default ProductComponent;
 
 const Wrap = styled.div`
 	background: #fff;
+	border-top-left-radius: 5px;
+	border-top-right-radius: 5px;
+	box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.5);
+	transition: var(--sht-trans);
+
+	:hover {
+		transform: scale(1.01);
+	}
+
 	.product__image {
 		object-fit: contain;
-		/* width: 15vw; */
 	}
 
 	.product__details {
@@ -95,6 +117,20 @@ const Wrap = styled.div`
 		.product__price {
 			color: #000;
 			font-weight: bold;
+		}
+
+		.star__rating {
+			font-size: 15px;
+		}
+
+		.product__link {
+			text-decoration: none;
+		}
+
+		.product__more {
+			background: var(--pry-clr-1);
+			color: #fff;
+			font-size: 13px;
 		}
 	}
 `;
