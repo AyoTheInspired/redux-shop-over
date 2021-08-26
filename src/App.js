11d993816532e449362347e2663e7a54b8b4d2d2
Ayo Abimbola <ayoinspired@gmail.com>
2021-08-27 00:17:30 +0100

    Clean up on App file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
-import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ProductListing from "./components/ProductListing";
 import ProductDetail from "./components/ProductDetail";
@@ -15,7 +14,7 @@ function App() {
 				<Navigation />
 				<Switch>
 					<Route path="/" exact component={ProductListing} />
-					{/* <Route path="/product/:productId" exact component={ProductDetail} /> */}
+					<Route path="/product/:productId" exact component={ProductDetail} />
 					{/* <Route>404 Not Found</Route> */}
 				</Switch>
 			</Router>
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,6 +1,12 @@
 import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function ProductDetail() {
+	const { productId } = useParams();
+
+	console.log(productId);
+
 	return (
 		<div>
 			<h1>Product Detail</h1>
