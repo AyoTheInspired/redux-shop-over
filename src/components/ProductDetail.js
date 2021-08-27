864f43fb7d977015439a2bcac9165e444cf52bca
Ayo Abimbola <ayoinspired@gmail.com>
2021-08-27 14:02:08 +0100

    More stylings

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -34,17 +34,17 @@ function ProductDetail() {
 	return (
 		<Container>
 			<Row>
-				<Section className="flexed flex-wrap py-5 bg-secondary my-4">
+				<Section className="flexed flex-wrap py-5 my-4">
 					<Col
 						sm={10}
 						md={5}
 						lg={5}
-						className="singleProduct__img p-2 d-flex flex-column">
+						className="singleProduct__imgWrap py-2 px-3 d-flex flex-column">
 						<img
 							src={singleProduct.image}
-							width="200"
+							// width="300"
 							alt={singleProduct.title}
-							className="mx-auto"
+							className="mx-auto m-2 singleProduct__img"
 						/>
 						<div className="singleProduct__bottom mt-3 col">
 							<p className="mb-0 single__product-title">
@@ -60,13 +60,13 @@ function ProductDetail() {
 						sm={10}
 						md={5}
 						lg={5}
-						className="singleProduct__desc bg-warning d-flex flex-column p-3">
-						<p className="mb-0 text-center text-capitalize">
+						className="singleProduct__details p-2 d-flex flex-column">
+						<p className="mb-3 px-3 text-center flexed text-capitalize singleProduct-description">
 							{singleProduct.description}{" "}
 						</p>
 
-						<div className="flexed p-2">
-							<p className="singleProduct-category text-capitalize">
+						<div className="flexed singleProduct__bottomRight mx-auto p-2">
+							<p className="singleProduct-category mb-0 text-capitalize">
 								{singleProduct.category}
 							</p>
 
@@ -85,11 +85,19 @@ export default ProductDetail;
 
 const Section = styled.section`
 	/* min-height: calc(100vh - 44px); */
-
-	.singleProduct__img {
+	background: var(--pry-clr-2);
+	.singleProduct__imgWrap,
+	.singleProduct__details {
 		background: #fff;
 		box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.4);
-		/* border- */
+	}
+
+	.singleProduct__img {
+		width: 70%;
+
+		@media (max-width: 768px) {
+			/* width: 50px !important; */
+		}
 	}
 
 	.singleProduct__bottom {
@@ -112,5 +120,33 @@ const Section = styled.section`
 		font-weight: 500;
 		border-radius: 4px;
 		box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.5);
+	}
+
+	.singleProduct__details {
+		justify-content: space-evenly;
+		height: 100%;
+
+		@media (max-width: 450px) {
+			height: 50%;
+		}
+
+		.singleProduct__bottomRight {
+			min-width: 50%;
+			background: #ddd;
+			display: flex;
+			justify-content: space-evenly;
+			align-items: center;
+			box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.5);
+		}
+		.singleProduct-description {
+			/* flex: 1; */
+			line-height: 1.5rem;
+			font-size: 14px;
+		}
+
+		.singleProduct__cart {
+			color: var(--pry-clr-1);
+			font-size: 20px;
+		}
 	}
 `;
